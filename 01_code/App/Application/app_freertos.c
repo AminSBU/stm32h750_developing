33fea68eee494370bb68f14a6d3422c77f23b9b0
LAPTOP-8L1MTC96\Afra <omid.elc88@gmail.com>
2026-02-27 17:37:08 +0330

    toggle LED every 1s

---
+++ 01_code/App/Application/app_freertos.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+void ledTaskFunction(void const * argument)
+{
+  /* USER CODE BEGIN ledTaskFunction */
+  /* Infinite loop */
+  for(;;)
+  {
+	  HAL_GPIO_TogglePin(LED_GPIO_Port, LED_PIN);
+    osDelay(1000);
+  }
+  /* USER CODE END ledTaskFunction */
+}
